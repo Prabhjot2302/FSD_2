28b575d2c298749e627eef557e1a50d704ff0ba7
prabhjot kaur <162884187+Prabhjot2302@users.noreply.github.com>
2026-01-20 09:54:45 +0530

    added exp-2

---
+++ Experiment-2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SinglePageApp from './components/Spa'
+
+function App() {
+
+  return (
+    <>
+      <SinglePageApp></SinglePageApp>
+    </>
+  )
+}
+
+export default App
